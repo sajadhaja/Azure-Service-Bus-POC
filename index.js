fd7e1a958a0ed805f78c3b6e173a2199b270a6c1
sajadhaja <sajadhaja90@gmail.com>
2017-11-07 19:08:07 +0530

    ddd

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 1337;
+/*var port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running at http://localhost:%d", port);*/
+
+
+
+var port = process.env.NODE_ENV === 'production' ? 80 : 4000;
+var server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
