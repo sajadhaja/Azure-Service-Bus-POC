57100fce750a20e995d9785e9b781b047f40eb75
sajadhaja <sajadhaja90@gmail.com>
2017-11-07 18:50:37 +0530

    d

--- createSubscriber.js
+++ createSubscriber.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var azure = require('azure');
 
-
+ 
 //var connString="Endpoint=sb://testservicebusneu.servicebus.windows.net/;SharedAccessKeyName=<key-name>;SharedAccessKey=<key>"
 var connString ="Endpoint=sb://testservicebusneu.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=365u7ACwz0IByTvZKgB9b7fIIX3o0cAZKyexg49mfmA="
 var serviceBusService = azure.createServiceBusService(connString);
